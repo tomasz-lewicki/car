1eccc588ed92d5918d0aab963038276601d55761
Tomasz Lewicki <t.w.lewicki@gmail.com>
2019-05-12 22:37:49 -0700

    new radar node

--- radar_node/main.py
+++ radar_node/main.py
@@ -1,29 +1,29 @@
 import numpy as np
-import json
+import jsonpickle
 from clustering import clusters
 from matplotlib import pyplot as plt
-
+import time
 import rospy
 from std_msgs.msg import String
 from sensor_msgs import point_cloud2
 from sensor_msgs.msg import PointCloud2
 from rospy.numpy_msg import numpy_msg
 
-def callback_pointcloud(data):
-
-    assert isinstance(data, PointCloud2)
-
-    gen = point_cloud2.read_points(data)
+def callback_pointcloud(msg):
+    assert isinstance(msg, PointCloud2)
+    #if msg.header.seq % 2 == 0: return
+    gen = point_cloud2.read_points(msg)
 
     array = np.array(list(gen))
-
+    array[:,1] = - array[:,1]
     cluster_dict = clusters(array)
 
 
     # for k, v in cluster_dict.items():
     #     print(type(v))
 
-    pub.publish(json.dumps(cluster_dict))
+    pub.publish(jsonpickle.encode(cluster_dict))
+
 
     # print(cluster_dict['no_clusters'])
     # cluster_dict['centroids']
@@ -36,6 +36,6 @@ if __name__ == '__main__':
     rospy.init_node('radar_pipeline', anonymous=True)
 
     rospy.Subscriber("/ti_mmwave/radar_scan_pcl", PointCloud2, callback_pointcloud)
-    pub = rospy.Publisher("/radar_pipeline/clusters", String, queue_size=10)
+    pub = rospy.Publisher("/radar_pipeline/clusters", String, queue_size=0)
     rospy.spin()
 
